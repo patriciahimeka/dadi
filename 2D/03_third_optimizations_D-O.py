f6541d8a16cc5e8813aad90e72264bae8e9a04a3
Patricia Wepfer <patwepfer@gmail.com>
2021-02-09 17:04:16 +0100

    Update and rename Da-Ok_03_third_opt.py to 03_third_optimizations_D-O.py

--- 2D/03_third_optimizations_D-O.py
+++ 2D/03_third_optimizations_D-O.py
@@ -8,23 +8,20 @@ import Optimize_Functions
 from datetime import datetime
 
 '''
-usage: python dadi_2D_03_third_optimizations.py
+Third round of optimaztion for model fitting
+Populations Daito (D) and Ryukyu (O)
 
-daniel.portik@uta.edu -> danielportik@email.arizona.edu
-October 2017
+usage: python 2D_03_third_optimizations_D-O.py
+Requires the Models_2D.py and Optimize_Functions.py scripts to be in same working directory. 
+
+Scripts adapted from Dan Portik: https://github.com/dportik/dadi_pipeline
+
+Patricia Wepfer
+August 2018
 '''
 #keep track of start time
 t_begin = datetime.now()
 
-
-#===========================================================================
-#get snps file and convert into allele frequency spectrum object in dadi
-
-#**************
-#snps1 = "/FULL PATH TO /dadi_2pops_North_South_snps.txt"
-
-#Create python dictionary from snps file
-#dd1 = dadi.Misc.make_data_dict(snps1)
 
 #**************
 #pop_ids is a list which should match the populations headers of your SNPs file columns
@@ -62,31 +59,6 @@ sym_mig_params = [1.0059,	1.8537,	2.2456,	2.9731]
 #5 Values
 asym_mig_params = [0.5353,	1.5052,	4.985,	2.3124,	1.8503]
 
-#"anc_sym_mig"
-#5 Values
-anc_sym_mig_params = [0.7858,	1.2965,	3.8012,	2.6331,	0.0154]
-
-#"anc_asym_mig"
-#6 Values
-anc_asym_mig_params = [2.0371,	6.303,	2.3277,	0.6339,	9.9115,	0.0768]
-
-#"sec_contact_sym_mig"
-#5 Values
-sec_contact_sym_mig_params = [1.4501,	2.7931,	1.5826,	0.3157,	5.3692]
-
-#"sec_contact_asym_mig"
-#6 Values
-sec_contact_asym_mig_params = [0.3108,	2.5649,	10.76,	0.7624,	5.7009,	0.5631]
-
-#sym_mig_two_epoch
-#6	values
-sym_mig_twoepoch_params =	[2.0384,	3.8796,	0.6922,	1.0689,	0.2146,	7.4406]
-
-#asym_mig_two_epoch
-#8	values
-asym_mig_twoepoch_params =[0.4944,	1.6558,	13.9356,	3.5927,	5.1233,	2.0408,	0.9365,	0.9516]
-
-
 
 #======================================================================================
 #Input some of the basic reusable arguments here specific to your data set
@@ -116,33 +88,12 @@ Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "no_divergen
 # Split into two populations, no migration.
 Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "no_mig", no_mig_params)
 
-
 # Split into two populations, with continuous symmetric migration.
 Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "sym_mig", sym_mig_params)
 
 # Split into two populations, with continuous asymmetric migration.
 Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "asym_mig", asym_mig_params)
 
-
-# Split with continuous symmetric migration, followed by isolation.
-Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "anc_sym_mig", anc_sym_mig_params)
-
-# Split with continuous asymmetric migration, followed by isolation.
-Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "anc_asym_mig", anc_asym_mig_params)
-
-
-# Split with no gene flow, followed by period of continuous symmetrical gene flow.
-Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "sec_contact_sym_mig", sec_contact_sym_mig_params)
-
-# Split with no gene flow, followed by period of continuous asymmetrical gene flow.
-Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "sec_contact_asym_mig", sec_contact_asym_mig_params)
-
-
-# Split into two populations, with continuous symmetric migration, rate varying across two epochs.
-Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "sym_mig_twoepoch", sym_mig_twoepoch_params)
-
-# Split into two populations, with continuous asymmetric migration, rate varying across two epochs.
-Optimize_Functions.Optimize_Round3(pts, fs, outfile, reps, maxiter, "asym_mig_twoepoch", asym_mig_twoepoch_params)
 
 #===========================================================================
 #clock the amount of time to complete the script
